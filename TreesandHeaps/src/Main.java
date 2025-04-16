cb872b6a97495bce469cc9d11cd034c3e386c923
Daron <daronbaltazar@gmail.com>
2025-04-16 13:00:08 -0500

    Added Java files
    Added Java files

---
+++ TreesandHeaps/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String candidates[] =  {"Marcus Fenix", "Dominic Santiago", "Damon Baird", "Cole Train", "Anya Stroud"};
+        //castVote("Cole Train");
+        //castVote("Cole Train");
+        //castVote("Marcus Fenix");
+        //castVote("Anya Stroud");
+        //castVote("Anya Stroud");
+        //rigElection("Marcys Fenix");
+
+    }
+}
